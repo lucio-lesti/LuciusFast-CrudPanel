aefa2e02fa7d3c3c4e62906ba6f70718c81599a2
lucio-lesti <40835369+lucio-lesti@users.noreply.github.com>
2022-06-28 10:21:12 +0200

    Update Mod_magazzino_tessere_model.php

--- application/modules/mod_magazzino_tessere/models/Mod_magazzino_tessere_model.php
+++ application/modules/mod_magazzino_tessere/models/Mod_magazzino_tessere_model.php
@@ -18,17 +18,12 @@ class Mod_magazzino_tessere_model extends BaseModel
 
 		//NOTE:NELLA FUNZIONE 'setFieldArrayGrid' INDICARE NEL VETTORE CHE SI COLLEGA ALLA TABELLA REFERENZIATA
 		//ALLA CHIAVE 'NOME', IL NOMINATIVO DEL CAMPO COLLEGATO
-
 		//NOTE 2: NELLA FUNZIONE 'setFieldArrayGrid' se nella chiave "nome" si usa una array, la classe "BaseModel" lo interpreta come un concat
 
 		$this->setFieldArrayGrid('nome', FIELD_STRING);
 		$this->setFieldArrayGrid('fk_affiliazione',FIELD_NUMERIC,'mod_affiliazioni',array("id" => 'id', "nome" => 'nome'));
 		$this->setFieldArrayGrid('id', FIELD_NUMERIC);
 		
-		//ESEMPIO DI TABELLA REFERENZIATA CHE NON HA IL CAMPO 'NOME'. QUI INDICHIAMO AL PROGRAMMA QUALE E' IL CAMPO DA USARE COME CAMPO 'NOME'
-		//P.S.QUESTA OPERAZIONE E' POSSIBILE FARLA ANCHE NEL METODO 'setFieldArrayGrid'
-		//$this->arrayColumnsReferenced['mod_sport']['nome'] = "sport"; 
-
 	}
 
 
